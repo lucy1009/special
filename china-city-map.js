e3ec3409aa633a4c61b91edae00cddb04d11935d
lucy1009 <smjanan@hotmail.com>
2025-06-29 08:16:18 +0800

    Create china-city-map.js

---
+++ china-city-map.js
@@ -0,0 +1,42 @@
+window.ChinaGeoMap = {
+  country: {
+    "China": "中国",
+    "United States": "美国",
+    "Japan": "日本",
+    "Russia": "俄罗斯",
+    "Germany": "德国",
+    "France": "法国",
+    "Canada": "加拿大",
+    "Australia": "澳大利亚",
+    "United Kingdom": "英国"
+  },
+  province: {
+    "Yunnan": "云南省",
+    "Beijing": "北京市",
+    "Shanghai": "上海市",
+    "Guangdong": "广东省",
+    "Zhejiang": "浙江省",
+    "Sichuan": "四川省",
+    "Shandong": "山东省",
+    "Jiangsu": "江苏省",
+    "Henan": "河南省",
+    "Hunan": "湖南省",
+    "Hubei": "湖北省",
+    "Anhui": "安徽省",
+    "Fujian": "福建省"
+  },
+  city: {
+    "Kunming": "昆明市",
+    "Beijing": "北京市",
+    "Shanghai": "上海市",
+    "Guangzhou": "广州市",
+    "Shenzhen": "深圳市",
+    "Hangzhou": "杭州市",
+    "Chengdu": "成都市",
+    "Wuhan": "武汉市",
+    "Nanjing": "南京市",
+    "Xi'an": "西安市",
+    "Zhengzhou": "郑州市",
+    "Changsha": "长沙市"
+  }
+};
